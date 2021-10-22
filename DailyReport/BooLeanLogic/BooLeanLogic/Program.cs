0e40a57da76cc0e88123147c322b43f2bf2f1f48
THolmes18 <tanasmith96@gmail.com>
2021-10-22 00:15:25 -0600

    BooLean

---
+++ DailyReport/BooLeanLogic/BooLeanLogic/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooLeanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(true && false); //AND
+            //Console.WriteLine(true && true);
+            //Console.WriteLine(false && false);
+
+            //Console.WriteLine(true || true);
+            //Console.WriteLine(true || false); //only 1 needs to be true (OR)
+            //Console.WriteLine(false || false);
+
+            //Console.WriteLine(true == true);
+            //Console.WriteLine(false == true);
+            //Console.WriteLine(false == false); //equal to
+
+            //Console.WriteLine(true != true); //Not equal
+            //Console.WriteLine(true != false);
+            //Console.WriteLine(false != false);
+
+            //Console.WriteLine(true ^ true); // XOR operator if 1 is true but not equal
+            //Console.WriteLine(true ^ false);
+            //Console.WriteLine(false ^ false);
+
+            Console.ReadLine();
+        }
+    }
+}
