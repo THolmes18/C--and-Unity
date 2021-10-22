4c130470b5ed0c94d3d8874424887e75ff82486b
THolmes18 <tanasmith96@gmail.com>
2021-10-22 00:26:10 -0600

    Assignment Update

--- BooLean Logic/BooLean Logic Submission Assignment/BooLean Logic Submission Assignment/Program.cs
+++ BooLean Logic/BooLean Logic Submission Assignment/BooLean Logic Submission Assignment/Program.cs
@@ -11,6 +11,22 @@ namespace BooLean_Logic_Submission_Assignment
         static void Main(string[] args)
         {
             Console.WriteLine("What is your age?");
+            int age = 2 * 16;
+            Console.WriteLine(age);
+            Console.ReadLine();
+
+            Console.WriteLine("Have you ever had a DUI?");
+            Console.WriteLine(false && false);
+            Console.ReadLine();
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            int speed = 1 + 0;
+            Console.WriteLine(speed);
+
+            Console.WriteLine("Qualified?");
+            Console.WriteLine(true && true);
+            Console.ReadLine();
+
 
         }
     }
