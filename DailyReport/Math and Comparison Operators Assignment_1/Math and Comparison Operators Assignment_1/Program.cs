96c132f02b2ac28b16a9175e6368aa5565548bb4
THolmes18 <tanasmith96@gmail.com>
2021-10-21 23:51:54 -0600

    New Assignment

---
+++ DailyReport/Math and Comparison Operators Assignment_1/Math and Comparison Operators Assignment_1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math_and_Comparison_Operators_Assignment_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anonymous Income Comparison Program");
+            Console.ReadLine();
+
+            Console.WriteLine("Person 1");
+            Console.ReadLine();
+
+            Console.WriteLine("Hourly Rate?"); 
+            int hour1 = 5 * 3;
+            Console.WriteLine(hour1);
+            Console.ReadLine();
+
+            Console.WriteLine("Hours Worked per week?");
+            int week1 = 8 * 5;
+            Console.WriteLine(week1);
+            Console.ReadLine();
+
+            Console.WriteLine("Person 2");
+            Console.ReadLine();
+
+            Console.WriteLine("Hourly Rate?"); //int variable
+            int hour2 = 10 * 2;
+            Console.WriteLine(hour2);
+            Console.ReadLine();
+
+            Console.WriteLine("Hours Worked per week?");
+            int week2 = 8 * 5;
+            Console.WriteLine(week2);
+            Console.ReadLine();
+
+            Console.WriteLine("Annual Salary of Person 1");
+            int salary1 = 15 * 40 * 52;
+            Console.WriteLine(salary1);
+            Console.ReadLine();
+
+            Console.WriteLine("Annual Salary of Person 2");
+            int salary2 = 20 * 40 * 52;
+            Console.WriteLine(salary2);
+            Console.ReadLine();
+
+            Console.WriteLine("Does Person 1 make more than Person 2?");
+            bool salaryCompare = 31200 > 41600;
+            Console.Write(salaryCompare.ToString());
+            Console.ReadLine();
+
+
+        }
+    }
+}
--- MathAndComparisonOperatiors/MathAndComparisonOperatiors/Program.cs
+++ MathAndComparisonOperatiors/MathAndComparisonOperatiors/Program.cs
@@ -26,15 +26,35 @@ namespace MathAndComparisonOperatiors
             // Console.WriteLine(product);
             // Console.ReadLine();
 
+            //double quotient = 100.0 / 17.0;
+            //Console.WriteLine(quotient);
+            //Console.ReadLine()
+
             //division
-            int quotient = 100 / 5;
-            Console.WriteLine(quotient);
-            Console.ReadLine();
+            //int quotient = 100 / 5;
+            //Console.WriteLine(quotient);
+            //Console.ReadLine();
 
             //remainders
-            int remainder = 11 % 2;
-            Console.WriteLine(remainder);
+            //int remainder = 11 % 2;
+            //Console.WriteLine(remainder);
+            //Console.ReadLine();
+
+
+            //Comparison Operators  true or false
+            //bool trueOrFalse = 12 > 5;
+            //Console.Write(trueOrFalse.ToString());
+            //Console.ReadLine();
+
+            int roomTemperture = 70;
+            int currentTemperture = 72;
+
+            //bool isWarm = currentTemperture >= roomTemperture; //>= satisfys this as well if it would need to be greater than or equal too..
+            bool isWarm = currentTemperture == roomTemperture; //!= meant not equal == means equals
+            Console.WriteLine(isWarm);
             Console.ReadLine();
+
+     
         }
     }
 }
