7bfde747cf75643bb6b3555b4d5ed0b57b2777a9
THolmes18 <tanasmith96@gmail.com>
2021-10-23 17:12:52 -0600

    Update

--- Branching/Branching Submission Assignment1/Branching Submission Assignment1/Program.cs
+++ Branching/Branching Submission Assignment1/Branching Submission Assignment1/Program.cs
@@ -10,6 +10,36 @@ namespace Branching_Submission_Assignment1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+
+            Console.WriteLine("Please enter the package weight:");
+            int weight = Convert.ToInt32(Console.ReadLine());
+
+
+            if (weight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+            }
+            else
+            {
+                Console.WriteLine("Package width?");
+                int width = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Package height?");
+                int height = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Package length?");
+                int length = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Quote");
+                int Quote = height * width * length * weight / 100;
+                Console.WriteLine(Quote);
+                Console.ReadLine();
+
+
+
+            }
+
         }
     }
 }
