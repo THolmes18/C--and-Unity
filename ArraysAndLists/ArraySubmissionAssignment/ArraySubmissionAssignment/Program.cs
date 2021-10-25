98d94efe9a972a3bf56a86dd1dcabf79684d7449
THolmes18 <tanasmith96@gmail.com>
2021-10-25 17:34:06 -0600

    assignment

--- ArraysAndLists/ArraySubmissionAssignment/ArraySubmissionAssignment/Program.cs
+++ ArraysAndLists/ArraySubmissionAssignment/ArraySubmissionAssignment/Program.cs
@@ -2,24 +2,43 @@
 using System.Collections.Generic;
 
 
-    class Program
+class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        string[] stringArray = { "Hello!", "How's it going?", "Who are you?", "Where are you located?" }; //stringArray object
+        Console.WriteLine("Please select a number (0-3)."); //asking the users
+        int userInput = Convert.ToInt32(Console.ReadLine()); //Users input set to a variable.
+
+        if (userInput <= 3 && userInput >= 0)
         {
+            Console.WriteLine(stringArray[userInput]); //user display index value
 
-        int[]yearArray = new int[5];  //declaring array
-        yearArray[0] = 1996;
-        yearArray[1] = 2015;
-        yearArray[2] = 2018;
-        yearArray[3] = 2019;
-        yearArray[4] = 2020;
-
-        yearArray[4] = 2019;
-
-        string[] lifeLine = { "Born", "Graudated HS", "Graduated College", "Had a Daughter", "Had a Son" };
-
-        Console.WriteLine(yearArray[4]);
+        }
+        else
+        {
+            Console.WriteLine("Value doesn't exist at index.");
+        }
         Console.ReadLine();
+
+        int[] numArray = new int[] { 10, 20, 40, 60, 80 };
+        Console.WriteLine("Please select a number (0-4).");
+        int userSelection = Convert.ToInt32(Console.ReadLine());
+
+        if (userSelection <=4 && userSelection >= 4)
+        {
+            Console.WriteLine(numArray[userSelection]);
+        }
+        else
+        {
+            Console.WriteLine("Selected index does not exist.");
+        }
+        
+        Console.ReadLine();
+        
+        
+        
+        
 
     }
 }
@@ -41,7 +60,7 @@ using System.Collections.Generic;
 
 ////shortcut
 ////int[] numArray1 = new int[] { 5, 2, 10, 200, 5000 };
-////another shortcute
+////another shortcut
 
 //int[] numArray2 = { 5, 2, 10, 200, 5000, 600, 2300 };
 
