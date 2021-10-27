e6b2d7a7217e787f893f5a20c00ed1ef121517f5
THolmes18 <tanasmith96@gmail.com>
2021-10-26 19:54:14 -0600

    updated

--- Try-Catch/Try-Catch/Program.cs
+++ Try-Catch/Try-Catch/Program.cs
@@ -4,7 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-      
+        Console.WriteLine("Guess a number?");
+        int number = Convert.ToInt32(Console.ReadLine());
+        bool isGuessed = false;
+        do //add before while and it will correct the while error.
+        {
+            //switch statements starts with switch (what you want to check)
+            switch (number)
+            {
+                case 62: //if number = 62
+                    Console.WriteLine("You Guessed 62. Try again.");
+                    Console.WriteLine("Guess a number?");
+                    number = Convert.ToInt32(Console.ReadLine());
+                    break;
+
+                case 29:
+                    Console.WriteLine("You guessed 29. Try again.");
+                    Console.WriteLine("Guess a number?");
+                    number = Convert.ToInt32(Console.ReadLine());
+                    break;
+
+
+                case 55:
+                    Console.WriteLine("You guessed 55. Try again.");
+                    Console.WriteLine("Guess a number?");
+                    number = Convert.ToInt32(Console.ReadLine());
+                    break;
+
+                case 12:
+                    Console.WriteLine("You guessed 12. That is correct.");
+                    isGuessed = true;
+                    break;
+
+                default: //if no case is guessed.
+                    Console.WriteLine("You are wrong.");
+                    Console.WriteLine("Guess a number?");
+                    number = Convert.ToInt32(Console.ReadLine());
+                    break;
+            }
+
+        }
+        while (!isGuessed); //good to avoid while  loops it can be endless
+
+        Console.Read();
 
 
         try
