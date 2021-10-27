87085ee96df0dfa2cc48ea52bab813ed9a214576
THolmes18 <tanasmith96@gmail.com>
2021-10-27 16:50:23 -0600

    Assignment

---
+++ CallingMethodsSubmission/CallingMethodsSubmission/MethodsCreated.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsSubmission
+{
+    public class MethodsCreated
+    {
+        public void methodOne(int input) //void = do not need the return method
+        {
+            Console.WriteLine(input + 100);
+        }
+    
+        public void methodTwo(int input_2)
+        {
+            Console.WriteLine(input_2 / 100);
+        }
+     
+    
+        public void methodThree(int input_3)
+        {
+          Console.WriteLine(input_3 * 100);
+        }
+
+    
+    }
+}
+
---
+++ CallingMethodsSubmission/CallingMethodsSubmission/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsSubmission
+{
+    class Program
+    {
+        static void Main(string[] args) //void= return nothing change to interger for a return
+        {
+            MethodsCreated methodTwo = new MethodsCreated(); //use class named method created and create an object to use the properties throughout
+
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            methodTwo.methodOne(input); //calling method user input for arguement
+            Console.ReadLine();
+
+
+            MethodsCreated methodThree = new MethodsCreated(); //use class named method created and create an object to use the properties throughout
+
+            int input_2 = Convert.ToInt32(Console.ReadLine());
+
+            methodThree.methodTwo(input_2); //calling method user input for arguement
+            Console.ReadLine();
+
+            MethodsCreated methodOne = new MethodsCreated(); //use class named method created and create an object to use the properties throughout
+
+            int input_3 = Convert.ToInt32(Console.ReadLine());
+
+            methodOne.methodThree(input_3); //calling method user input for arguement
+            Console.ReadLine();
+
+        }
+    
+      
+      
+
+      
+    }
+}
+
