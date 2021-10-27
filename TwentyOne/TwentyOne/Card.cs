80295a9a79faea514fb317666813bfb3ce59b04c
THolmes18 <tanasmith96@gmail.com>
2021-10-27 13:17:15 -0600

    21

---
+++ TwentyOne/TwentyOne/Card.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    public class Card
+    {
+        public Card() //constructor values assigned to an object upon creation
+        {
+            Suit = "Spades";
+            Face = "Two";
+        }
+        public string Suit { get; set; } //The card class has a property of data type string called suit and you can get or set this property. Public means other parts of the program can use it as well
+        public string Face { get; set; }
+    }
+}
+
+//Try 1 class to 1 file
+//properties = state (suit) get and set only thing you can do
+//class is a design for something--a model
+//value goes to object
+//class= cookie cutter object= cookie
