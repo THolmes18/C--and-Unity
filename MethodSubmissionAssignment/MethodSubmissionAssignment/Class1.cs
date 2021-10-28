1ffb0d6915b75f4ad691b766080ace6720a8f710
THolmes18 <tanasmith96@gmail.com>
2021-10-28 13:53:16 -0600

    class

---
+++ MethodSubmissionAssignment/MethodSubmissionAssignment/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace MethodSubmissionAssignment
+{
+    class Class1
+    {
+
+        public static void Add(int a, int b, [ Optional ] int[] n)
+        {
+            int Sum = a + b;
+            if (n != null)
+            {
+                foreach (int no in n)
+                {
+                    Sum += no;
+                }
+            }
+            Console.WriteLine("Sum is {0}: ", Sum);
+        }
+
+        private class OptionalAttribute : Attribute
+        {
+        }
+
+
+
+        //public int Add(int a, int b, [ Optional ] int[] n)
+        //{
+        //    int sum = a + b;
+        //    return sum;
+    }
+    
+}
+
---
+++ MethodSubmissionAssignment/MethodSubmissionAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Please input a number.");
+            int sum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please input a second number.");
+            int sum2 = Convert.ToInt32(Console.ReadLine());
+            
+            Class1 class1 = new Class1(); //object
+                                          //call Class2 method
+            Console.WriteLine(class1.Add(sum, sum2));
+            Console.ReadLine();
+        }
+    }
+}
