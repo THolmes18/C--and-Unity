498b38d448f9eaa959e88c447db4d91785cc2ec5
THolmes18 <tanasmith96@gmail.com>
2021-10-27 19:44:32 -0600

    New

---
+++ MainMethodAssignment/MainMethodAssignment/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class MainMethod
+    {
+        
+        public void methods(int a) //void = do not need the return method ...this will have one parameter
+        {
+            Console.WriteLine("Arguements: " + a);
+        }
+
+        public void methods(int a, int b) //2 parameters
+        {
+            Console.WriteLine("Arguements:" + a + " and " + b);
+        }
+
+
+        public void methods(int a, int b, int c) //3 parameters
+        {
+            Console.WriteLine("Arguements:" + a + " and " + b + " and " + c);
+        }
+    }
+}
---
+++ MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+       public static void Main(string[] args)
+        {
+            MainMethod methods = new MainMethod(); //use class named method created and create an object to use the properties throughout
+            methods.methods(100);
+            methods.methods(100, 200);
+            methods.methods(100, 200, 300);
+            Console.ReadLine();
+
+        }
+    }
+}
