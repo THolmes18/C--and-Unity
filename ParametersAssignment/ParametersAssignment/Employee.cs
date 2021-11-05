158a3a9bd07d46e71b365186a8e600641678297d
THolmes18 <tanasmith96@gmail.com>
2021-11-05 15:32:51 -0600

    assignments

---
+++ ParametersAssignment/ParametersAssignment/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParametersAssignment
+{
+    public class Employee<T>
+    {
+        public List<T> Things { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+        public void Name(string FirstName)
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+        }
+    }
+}
+
+
+
+
+
---
+++ ParametersAssignment/ParametersAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParametersAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<T> employee = new Employee<T>() { FirstName = "T'Ana", LastName = "Holmes" };
+            employee.Things = new List<T>();
+            Console.ReadLine();
+
+
+        }
+    }
+}
+//generic 3 Player<Card> player = new Player<Card>();
+//generic 4 player.Hand = new List<Card>(); (Generics is primarily used for List
