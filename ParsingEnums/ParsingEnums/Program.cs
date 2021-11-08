1f7ccbb64c6a75e6fc48bb7e15f49f204c79df0b
THolmes18 <tanasmith96@gmail.com>
2021-11-08 14:32:03 -0700

    Weekly

---
+++ ParsingEnums/ParsingEnums/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            try 
+            {
+                Weekly weekly = new Weekly();
+                Console.WriteLine("Please input the current Day of the Week.", Enum.Parse(DaysOfTheWeek);
+            }
+            
+            catch (Exception e)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ParsingEnums/ParsingEnums/Weekly.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnums
+{
+    class Weekly
+    {
+        public enum DaysOfTheWeek
+        {
+            Monday=2,
+            Tuesday=4,
+            Wednesday=6,
+            Thursday=8,
+            Friday=10,
+            Saturday=12,
+            Sunday=14
+        }
+    }
+}
