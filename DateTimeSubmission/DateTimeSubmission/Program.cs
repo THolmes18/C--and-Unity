b92edebd96125f8d27477c4bd4ccbe9b4851b52f
THolmes18 <tanasmith96@gmail.com>
2021-11-10 14:32:51 -0700

    DateTime New

--- DateTimeSubmission/DateTimeSubmission/Program.cs
+++ DateTimeSubmission/DateTimeSubmission/Program.cs
@@ -16,6 +16,18 @@ namespace DateTimeSubmission
 
            
             Console.WriteLine("Please pick a number.");
+
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            DateTime.Now.AddHours(userInput );
+
+
+            DateTime d1 = new DateTime(2021, 11, 10, 2, 31, 10);
+            DateTime d2 = d1.AddHours(userInput);
+            System.Console.WriteLine("Initial DateTime = {0:dd} {0:y}, {0:t} ", d1);
+            System.Console.WriteLine("\nNew DateTime (After adding hours) = {0:dd} {0:y}, {0:t} ", d2);
+
+
+
             Console.ReadLine();
         }
     }
