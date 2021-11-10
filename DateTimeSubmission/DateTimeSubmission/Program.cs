dcc31550804ac31b4aae75fe03767308ab22894a
THolmes18 <tanasmith96@gmail.com>
2021-11-10 14:58:49 -0700

    DateTime Updated

--- DateTimeSubmission/DateTimeSubmission/Program.cs
+++ DateTimeSubmission/DateTimeSubmission/Program.cs
@@ -21,12 +21,9 @@ namespace DateTimeSubmission
             DateTime.Now.AddHours(userInput );
 
 
-            DateTime d1 = new DateTime(2021, 11, 10, 2, 31, 10);
-            DateTime d2 = d1.AddHours(userInput);
-            System.Console.WriteLine("Initial DateTime = {0:dd} {0:y}, {0:t} ", d1);
-            System.Console.WriteLine("\nNew DateTime (After adding hours) = {0:dd} {0:y}, {0:t} ", d2);
-
-
+            
+            DateTime d2 = now.AddHours(userInput);
+            Console.WriteLine("\nNew DateTime (After adding hours) = {0:dd} {0:y}, {0:t} ", d2);
 
             Console.ReadLine();
         }
