6c6311c5dafc8a3c48816d171fc1483ee3f55aa4
THolmes18 <tanasmith96@gmail.com>
2021-11-10 13:12:09 -0700

    DateTime

---
+++ DateTimeSubmission/DateTimeSubmission/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DateTimeSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamWriter file = new StreamWriter(@"C:\Users\T'ana\Documents\GitHub\C--and-Unity\DateTimeSubmission\log.txt", true))
+            file.WriteLine(DateTime.Now);
+        }
+    }
+}
