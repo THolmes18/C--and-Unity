dabca9716ea6c0706f1a8a53f07ace891c79db38
THolmes18 <tanasmith96@gmail.com>
2021-11-09 18:48:53 -0700

    main

--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -16,7 +16,7 @@ namespace TwentyOne
             int bank = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Hello, {0}. Would you like to join a game of 21 right now?", playerName);
             string answer = Console.ReadLine().ToLower();
-            if (answer == "yes" || answer == "yeah" || answer = "y" || answer = "ya")
+            if (answer == "yes" || answer == "yeah" || answer == "y" || answer == "ya")
             {
                 Player player = new Player(playerName, bank);
                 Game game = new TwentyOneGame();
@@ -26,7 +26,11 @@ namespace TwentyOne
                 {
                     game.Play();
                 }
+                game -= player;
+                Console.WriteLine("Thank you for playing!");
             }
+            Console.WriteLine("Feel free to look around the casino. Bye for now.");
+            Console.Read();
 
         }
     }
