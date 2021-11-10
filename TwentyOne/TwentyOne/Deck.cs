1de371bc572d39843c073d9217e83786edb969fd
THolmes18 <tanasmith96@gmail.com>
2021-11-09 22:03:53 -0700

    Update Deck.cs

--- TwentyOne/TwentyOne/Deck.cs
+++ TwentyOne/TwentyOne/Deck.cs
@@ -9,28 +9,6 @@ namespace TwentyOne
     public class Deck
     {
         public Deck() //constructor ALWAYS goes on top before property //method called as soon as object is created
-
-        {
-            Cards = new List<Card>();
-            List<string> Suits = new List<string>() { "Clubs", "Hearts", "Diamonds", "Spades" };
-            List<string> Faces = new List<string>()
-            {
-                "Two", "Three", "Four", "Five", "Six", "Seven",
-                "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"
-            };
-
-            foreach (string face in Faces)  //loop through each face 4 times1 for each suit (nested foreach loop)
-            {
-                foreach (string suit in Suits)
-                {
-                    Card card = new Card(); //when loop ends theis variable does not exist
-                    card.Suit = suit;
-                    card.Face = face;
-                    Cards.Add(card);
-                }
-            }
-
-        }
         {
             Cards = new List<Card>();
 
@@ -44,11 +22,9 @@ namespace TwentyOne
                     Cards.Add(card);
                 }
             }
-         }
-
-
+        }
         public List<Card> Cards { get; set; } //property
-   
+
         public void Shuffle(int times = 1) //static (which would go after public) calls method w/o applying to specific deck
         {
             for (int i = 0; i < times; i++)
@@ -63,10 +39,10 @@ namespace TwentyOne
                     Cards.RemoveAt(randomIndex);
                 }
                 Cards = TempList; //this means it is referring to itself; its own object
-            }        
+            }
         }
-    }     
-}    
+    }
+}
 
 
 //Constructor method is always the same name as the class it is in.
